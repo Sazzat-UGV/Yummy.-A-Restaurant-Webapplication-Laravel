10e3f14e9c62e0f9f0bb7f23c3a8548feb8f97c3
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2023-05-04 23:14:30 +0600

    showing data to dashboard

--- app/Http/Controllers/backend/dashboardController.php
+++ app/Http/Controllers/backend/dashboardController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers\backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\chef;
+use App\Models\contact;
+use App\Models\Product;
+use App\Models\category;
+use App\Models\bookTable;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class dashboardController extends Controller
 {
     public function dashboard(){
-        return view('backend.pages.dashboard');
+
+        $total_category=category::count();
+        $total_food_item=Product::count();
+        $total_chefs=chef::count();
+        $reservation_confirm=bookTable::where('status',1)->count();
+        $reservations=bookTable::latest('id')->select('id','name','phone','date','status')->limit(4)->get();
+
+        return view('backend.pages.dashboard',compact(
+            'total_category',
+            'total_food_item',
+            'total_chefs',
+            'reservation_confirm',
+            'reservations',
+        ));
     }
 }
--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -2,6 +2,9 @@
 @section('title')
 Dashboard
 @endsection
+@push('admin_style')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endpush
 
 @section('content')
 
@@ -9,33 +12,20 @@ Dashboard
 
 <section class="section dashboard">
     <div class="row">
-          <!-- Sales Card -->
-          <div class="col-xxl-4 col-md-4">
+
+          <div class="col-xxl-3 col-md-3">
             <div class="card info-card sales-card">
 
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div>
-
               <div class="card-body">
-                <h5 class="card-title">Sales <span>| Today</span></h5>
+                <h5 class="card-title">Category</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-cart"></i>
+                    <i class="fa-solid fa-list"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>145</h6>
-                    <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                    <h6>{{ $total_category }}</h6>
+                    <span class="text-muted small pt-2 ps-1">Category</span>
 
                   </div>
                 </div>
@@ -44,33 +34,19 @@ Dashboard
             </div>
           </div><!-- End Sales Card -->
 
-          <!-- Revenue Card -->
-          <div class="col-xxl-4 col-md-4">
+
+          <div class="col-xxl-3 col-md-3">
             <div class="card info-card revenue-card">
-
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div>
-
               <div class="card-body">
-                <h5 class="card-title">Revenue <span>| This Month</span></h5>
+                <h5 class="card-title">Total Food Item</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-currency-dollar"></i>
+                    <i class="fa-solid fa-list"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>$3,264</h6>
-                    <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                    <h6>{{ $total_food_item }}</h6>
+                    <span class="text-muted small pt-2 ps-1">Items</span>
 
                   </div>
                 </div>
@@ -79,33 +55,41 @@ Dashboard
             </div>
           </div><!-- End Revenue Card -->
 
-          <!-- Customers Card -->
-          <div class="col-xxl-4 col-md-4">
+
+          <div class="col-xxl-3 col-md-3">
             <div class="card info-card customers-card">
 
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div>
-
               <div class="card-body">
-                <h5 class="card-title">Revenue <span>| This Month</span></h5>
+                <h5 class="card-title">Chefs</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                       <i class="bi bi-people"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>$3,264</h6>
-                    <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                    <h6>{{ $total_chefs }}</h6>
+                    <span class="text-muted small pt-2 ps-1">Chefs</span>
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+          <div class="col-xxl-3 col-md-3">
+            <div class="card info-card customers-card">
+
+              <div class="card-body">
+                <h5 class="card-title">Reservation <span class="text-success text-bold">| Active</span></h5>
+
+                <div class="d-flex align-items-center">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                    <i class="bi bi-card-list"></i>
+                  </div>
+                  <div class="ps-3">
+                    <h6>{{ $reservation_confirm }}</h6><span class="text-muted small pt-2 ps-1">Reservations</span>
 
                   </div>
                 </div>
@@ -114,8 +98,42 @@ Dashboard
             </div>
           </div>
     </div>
-<!-- End Left side columns -->
+
+    <div class="row">
+        <h4>Reservations </h4>
+        <hr>
+        <div class="col-12">
+            <div class="div">
+                <table class="table table-responsive">
+                    <tr>
+                        <th>#</th>
+                        <th>Client Name</th>
+                        <th>Phone</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                    </tr>
+                    @foreach ($reservations as $index=>$reservation)
+                    <tr>
+                        <td>{{ $index+1 }}</td>
+                        <td>{{ $reservation->name }}</td>
+                        <td>{{ $reservation->phone }}</td>
+                        <td>{{ $reservation->date }}</td>
+                        <td>
+                            @if ($reservation->status==0)
+                            <span class="badge bg-warning">Pending</span>
+                            @elseif ($reservation->status==1)
+                            <span class="badge bg-success">Confirmd</span>
+                            @elseif ($reservation->status==2)
+                            <span class="badge bg-danger">Rejected</span>
+                        @endif</td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
 
     </div>
   </section>
 @endsection
+
